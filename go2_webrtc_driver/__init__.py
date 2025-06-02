e9fbfeed135906ca2fb61f123a874e63666ec1c7
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2025-06-02 12:06:17 +0200

    Fix aiortc deps

--- go2_webrtc_driver/__init__.py
+++ go2_webrtc_driver/__init__.py
@@ -1,3 +1,7 @@
+# Monkey-patch aiortc.rtcdtlstransport.X509_DIGEST_ALGORITHMS to remove extra SHA algorithms
+# This causes Unity Go2 to use a newer syntax, which is not compatible with the current aiortc version.
+
+
 # Monkey-patch aioice.Connection to use a fixed username and password
 
 import aioice
@@ -14,3 +18,22 @@ class Connection(aioice.Connection):
 
 
 aioice.Connection = Connection  # type: ignore
+
+
+import aiortc
+from packaging.version import Version
+
+
+if Version(aiortc.__version__) == Version("1.10.0"):
+    X509_DIGEST_ALGORITHMS = {
+        "sha-256": "SHA256",
+    }
+    aiortc.rtcdtlstransport.X509_DIGEST_ALGORITHMS = X509_DIGEST_ALGORITHMS
+
+elif Version(aiortc.__version__) >= Version("1.11.0"):
+    from cryptography.hazmat.primitives import hashes
+
+    X509_DIGEST_ALGORITHMS = {
+        "sha-256": hashes.SHA256(),  # type: ignore
+    }
+    aiortc.rtcdtlstransport.X509_DIGEST_ALGORITHMS = X509_DIGEST_ALGORITHMS
